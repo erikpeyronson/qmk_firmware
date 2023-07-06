3935a32052f8fffc8b043ff7caf845ff80f3e8b2
Erik Peyronson <erik.peyronson@gmail.com>
2023-07-06 16:00:36 +0200

    Rename build.sh build_and_flash.sh and add flash command

--- tools/build_and_flash.sh
+++ tools/build_and_flash.sh
@@ -6,3 +6,9 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 
 qmk json2c -o $REPO_ROOT/keyboards/$KBD/keymaps/$KEYMAP/keymap_out.h $REPO_ROOT/keyboards/$KBD/keymaps/$KEYMAP/keymap_in.json
 qmk compile -kb $KBD/$VERSION -km $KEYMAP
+
+echo "Put first half in bootloader mode"
+qmk flash -kb $kbd/$version -km $keymap
+echo "Put second half in bootloader mode"
+qmk flash -kb $kbd/$version -km $keymap
+
