c9c7cd1575e090b16703092f14900be69d35a703
Erik Peyronson <erik.peyronson@gmail.com>
2023-07-06 17:15:03 +0200

    Add swedish layer with tapdance to enter/exit

--- keyboards/crkbd/keymaps/erikpeyronson/keymap.c
+++ keyboards/crkbd/keymaps/erikpeyronson/keymap.c
@@ -7,7 +7,18 @@ enum Layers{
   Sym
 };
 
+enum TapDanceActions {
+  TD_Q_SWE = 0,
+  TD_Q_BASE
+};
+
 #define MY_LT(layer, kc) LT(layer, kc)
 
-#include "keymap_out.h"
+// Tap Dance definitions
+tap_dance_action_t tap_dance_actions[] = {
+    // Tap once for W, twice for switch to swedish layer
+    [TD_Q_SWE] = ACTION_TAP_DANCE_LAYER_MOVE(KC_Q, Swe),
+    [TD_Q_BASE] = ACTION_TAP_DANCE_LAYER_MOVE(KC_Q, Base),
+};
 
+#include "keymap_out.h"
