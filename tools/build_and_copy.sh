b5cb2a6aec8eae8abddfd72ce11af63f1f182a39
Erik Peyronson <erik.peyronson@gmail.com>
2023-07-06 09:09:57 +0200

    Generate keymap from json during build time

--- tools/build_and_copy.sh
+++ tools/build_and_copy.sh
@@ -2,6 +2,11 @@
 KBD=crkbd
 VERSION=erik2g
 KEYMAP=erikpeyronson
+
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+#
+qmk json2c -o $REPO_ROOT/keyboards/$KBD/keymaps/$KEYMAP/keymap_out.h $REPO_ROOT/keyboards/$KBD/keymaps/$KEYMAP/keymap_in.json
 qmk compile -kb $KBD/$VERSION -km $KEYMAP
 
 
