fdc0ab49b2b1ab535aabe7c91eb9fc712870d56b
Erik Peyronson <erik.peyronson@gmail.com>
2023-07-10 12:15:31 +0200

    Add function keys

--- tools/build_and_flash.sh
+++ tools/build_and_flash.sh
@@ -4,6 +4,14 @@ VERSION=erik2g
 KEYMAP=erikpeyronson
 REPO_ROOT=$(git rev-parse --show-toplevel)
 
+SOURCE=$HOME/Downloads/$KEYMAP.json
+DEST=$REPO_ROOT/keyboards/$KBD/keymaps/$KEYMAP/keymap_in.json
+
+if [[ -f $SOURCE ]]; then
+  mv $SOURCE $DEST
+  echo "Moved $SOURCE to $DEST"
+fi
+
 qmk json2c -o $REPO_ROOT/keyboards/$KBD/keymaps/$KEYMAP/keymap_out.h $REPO_ROOT/keyboards/$KBD/keymaps/$KEYMAP/keymap_in.json
 qmk compile -kb $KBD/$VERSION -km $KEYMAP
 
@@ -11,4 +19,3 @@ echo "Put first half in bootloader mode"
 qmk flash -kb $KBD/$VERSION -km $KEYMAP
 # echo "Put second half in bootloader mode"
 # qmk flash -kb $KBD/$VERSION -km $KEYMAP
-
