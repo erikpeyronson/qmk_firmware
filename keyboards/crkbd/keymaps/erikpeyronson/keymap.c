53fe64dfd1fde3891f8235e30995463ddfd2ed72
Erik Peyronson <erik.peyronson@gmail.com>
2023-07-20 12:44:41 +0200

    Started transparent coloring

--- keyboards/crkbd/keymaps/erikpeyronson/keymap.c
+++ keyboards/crkbd/keymaps/erikpeyronson/keymap.c
@@ -1,8 +1,8 @@
 #include QMK_KEYBOARD_H
 
+#include "keyboard.h"
 #include "oled_driver.h"
 #include "quantum.h"
-#include "keyboard.h"
 
 enum Layers { Base = 0, Swe, Num, Sym, Nav, Etc };
 
@@ -13,13 +13,13 @@ enum TapDanceActions { TD_Q_SWE = 0, TD_Q_BASE };
 // Tap Dance definitions
 tap_dance_action_t tap_dance_actions[] = {
     // Tap once for W, twice for switch to swedish layer
-    [TD_Q_SWE]  = ACTION_TAP_DANCE_LAYER_MOVE(KC_Q, Swe),
+    [TD_Q_SWE] = ACTION_TAP_DANCE_LAYER_MOVE(KC_Q, Swe),
     [TD_Q_BASE] = ACTION_TAP_DANCE_LAYER_MOVE(KC_Q, Base),
 };
 
 void keyboard_post_init_user(void) {
-    rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
-    rgb_matrix_sethsv_noeeprom(HSV_OFF);
+  rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
+  rgb_matrix_sethsv_noeeprom(HSV_OFF);
 }
 
 #include "keymap_out.h"
@@ -40,9 +40,9 @@ uint8_t led_mappings[4][6] = {
 // clang-format on
 
 struct ColorBinding {
-    uint16_t red;
-    uint16_t green;
-    uint16_t blue;
+  uint16_t red;
+  uint16_t green;
+  uint16_t blue;
 };
 
 struct ColorBinding layer_color_mappings[] = {
@@ -50,28 +50,35 @@ struct ColorBinding layer_color_mappings[] = {
 };
 
 bool rgb_matrix_indicators_user() {
-    // for(int i = 0; i < 54; ++i) {
-    //   rgb_matrix_set_color(i, RGB_WHITE);
-    // }
-    uint16_t layer = get_highest_layer(layer_state | default_layer_state);
-    for (int row = 0; row < 4; ++row) {
-        for (int col = 0; col < 6; ++col) {
-            keypos_t current_key; // {.col = col, .row = row};
-            current_key.col = col;
-            current_key.row = row;
-            uint16_t kc     = keymap_key_to_keycode(layer, current_key);
+  // for(int i = 0; i < 54; ++i) {
+  //   rgb_matrix_set_color(i, RGB_WHITE);
+  // }
+  uint16_t layer = get_highest_layer(layer_state | default_layer_state);
+  for (int row = 0; row < 4; ++row) {
+    for (int col = 0; col < 12; ++col) {
+      keypos_t current_key;  // {.col = col, .row = row};
+      if (col > 5) {
+        current_key.col = 11 - col;
+      } else {
+        current_key.col = col;
+      }
+      current_key.row = row;
+      uint16_t kc = keymap_key_to_keycode(layer, current_key);
 
-            if (kc != KC_NO && kc != KC_TRANSPARENT) {
-                uint8_t led_index = led_mappings[row][col];
-                if (!is_keyboard_master()) {
-                  led_index += 27;
-                }
-                struct ColorBinding* rgb = &layer_color_mappings[layer];
-                rgb_matrix_set_color(led_index, rgb->red, rgb->green, rgb->blue);
-            }
+      if (kc != KC_NO && kc != KC_TRANSPARENT) {
+        uint8_t led_index = led_mappings[row][col];
+        // right hand
+        if (col > 6) {
+          led_index += 27;
         }
+        struct ColorBinding* rgb = &layer_color_mappings[layer];
+        rgb_matrix_set_color(led_index, rgb->red, rgb->green, rgb->blue);
+      }
     }
-    return false;
+  }
+  // uint8_t led_index = led_mappings[0][0] + 27;
+  // rgb_matrix_set_color(led_index, RGB_WHITE);
+  return false;
 }
 
 #endif
