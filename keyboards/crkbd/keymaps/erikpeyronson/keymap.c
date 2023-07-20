c35d8df1bdf1836838553609a97aa42aad975515
Erik Peyronson <erik.peyronson@gmail.com>
2023-07-20 13:25:37 +0200

    Single color works on both halves

--- keyboards/crkbd/keymaps/erikpeyronson/keymap.c
+++ keyboards/crkbd/keymaps/erikpeyronson/keymap.c
@@ -13,13 +13,26 @@ enum TapDanceActions { TD_Q_SWE = 0, TD_Q_BASE };
 // Tap Dance definitions
 tap_dance_action_t tap_dance_actions[] = {
     // Tap once for W, twice for switch to swedish layer
-    [TD_Q_SWE] = ACTION_TAP_DANCE_LAYER_MOVE(KC_Q, Swe),
+    [TD_Q_SWE]  = ACTION_TAP_DANCE_LAYER_MOVE(KC_Q, Swe),
     [TD_Q_BASE] = ACTION_TAP_DANCE_LAYER_MOVE(KC_Q, Base),
 };
 
 void keyboard_post_init_user(void) {
-  rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
-  rgb_matrix_sethsv_noeeprom(HSV_OFF);
+    rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
+    rgb_matrix_sethsv_noeeprom(HSV_OFF);
+
+    // Customise these values to desired behaviour
+    // debug_enable=true;
+    // debug_matrix=true;
+    // debug_keyboard=true;
+    // debug_mouse=true;
+}
+bool process_record_user(uint16_t keycode, keyrecord_t* record) {
+    // If console is enabled, it will print the matrix position and status of each key pressed
+#ifdef CONSOLE_ENABLE
+    uprintf("KL: kc: 0x%04X, col: %2u, row: %2u, pressed: %u, time: %5u, int: %u, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
+#endif
+    return true;
 }
 
 #include "keymap_out.h"
@@ -31,7 +44,8 @@ const int led_count = 46 + 7;
 int leds[] = {6, 6, 17, 18, 29, 30, 36, 37, 38, 41, 44, 45, 46};
 
 // clang-format off
-uint8_t led_mappings[4][6] = {
+uint8_t led_mappings[8][6] = {
+  // left side (master)
   {24, 23, 18, 17, 10, 9 },
   {25, 22, 19, 16, 11, 8 },
   {26, 21, 20, 15, 12, 7 },
@@ -40,9 +54,9 @@ uint8_t led_mappings[4][6] = {
 // clang-format on
 
 struct ColorBinding {
-  uint16_t red;
-  uint16_t green;
-  uint16_t blue;
+    uint16_t red;
+    uint16_t green;
+    uint16_t blue;
 };
 
 struct ColorBinding layer_color_mappings[] = {
@@ -50,35 +64,28 @@ struct ColorBinding layer_color_mappings[] = {
 };
 
 bool rgb_matrix_indicators_user() {
-  // for(int i = 0; i < 54; ++i) {
-  //   rgb_matrix_set_color(i, RGB_WHITE);
-  // }
-  uint16_t layer = get_highest_layer(layer_state | default_layer_state);
-  for (int row = 0; row < 4; ++row) {
-    for (int col = 0; col < 12; ++col) {
-      keypos_t current_key;  // {.col = col, .row = row};
-      if (col > 5) {
-        current_key.col = 11 - col;
-      } else {
-        current_key.col = col;
-      }
-      current_key.row = row;
-      uint16_t kc = keymap_key_to_keycode(layer, current_key);
+    uint16_t layer            = get_highest_layer(layer_state | default_layer_state);
 
-      if (kc != KC_NO && kc != KC_TRANSPARENT) {
-        uint8_t led_index = led_mappings[row][col];
-        // right hand
-        if (col > 6) {
-          led_index += 27;
+    for (int row = 0; row < 8; ++row) {
+        for (int col = 0; col < 6; ++col) {
+            keypos_t current_key = {.col = col, .row = row};
+            // current_key.col = col;
+            // current_key.row = row + slave_row_offset;
+            uint16_t kc     = keymap_key_to_keycode(layer, current_key);
+
+            if (kc != KC_NO && kc != KC_TRANSPARENT) {
+                uint8_t led_index;
+                if (!is_keyboard_master()) {
+                  led_index = led_mappings[row - 4][col] + 27;
+                } else {
+                  led_index = led_mappings[row][col];
+                }
+                struct ColorBinding* rgb = &layer_color_mappings[layer];
+                rgb_matrix_set_color(led_index, rgb->red, rgb->green, rgb->blue);
+            }
         }
-        struct ColorBinding* rgb = &layer_color_mappings[layer];
-        rgb_matrix_set_color(led_index, rgb->red, rgb->green, rgb->blue);
-      }
     }
-  }
-  // uint8_t led_index = led_mappings[0][0] + 27;
-  // rgb_matrix_set_color(led_index, RGB_WHITE);
-  return false;
+    return false;
 }
 
 #endif
