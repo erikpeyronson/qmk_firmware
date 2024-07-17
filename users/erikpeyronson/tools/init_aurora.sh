a3ff4594b99662814774bed59713b0c84c12d795
Erik Peyronson <erik.peyronson@gmail.com>
2024-07-17 14:17:23 +0200

    fix init_aurora.sh

--- users/erikpeyronson/tools/init_aurora.sh
+++ users/erikpeyronson/tools/init_aurora.sh
@@ -7,14 +7,13 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 SOURCE=$HOME/Downloads/$KEYMAP.json
 DEST=$REPO_ROOT/keyboards/$KBD/keymaps/$KEYMAP/keymap_in.json
 
-mv $SOURCE $DEST
-echo "Moved json keymap from $SOURCE to $DEST"
+# mv $SOURCE $DEST
+# echo "Moved json keymap from $SOURCE to $DEST"
 
-echo -n "Setting qmk defaults -kb $KBD/$VERSION -km $KEYMAP ..."
-qmk config user.keymap=$KEYMAP
-echo " done!"
-qmk config user.keyboard=$KBD/$VERSION
+# echo -n "Setting qmk defaults -kb $KBD/$VERSION -km $KEYMAP ..."
+# qmk config user.keymap=$KEYMAP
+# echo " done!"
+# qmk config user.keyboard=$KBD/$VERSION
 
-qmk json2c -o $REPO_ROOT/keyboards/$KBD/keymaps/$KEYMAP/keymap_out.h $REPO_ROOT/keyboards/$KBD/keymaps/$KEYMAP/keymap_in.json
+qmk json2c -o $REPO_ROOT/keyboards/$KBD/$VERSION/keymaps/$KEYMAP/keymap_out.h $REPO_ROOT/keyboards/$KBD/$VERSION/keymaps/$KEYMAP/keymap_in.json
 qmk generate-compilation-database
-
